79ec7aeb4570df6d71ec1ab6c0694909d30fff44
anudas <anudasr2@gmail.com>
2026-04-11 23:43:15 +0530

    Initialize frontend repository

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hero from '../components/sections/Hero';
+import About from '../components/sections/About';
+import Values from '../components/sections/Values';
+import ProductsSection from '../components/sections/ProductsSection';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <ProductsSection />
+      <About />
+      <Values />
+    </>
+  );
+}
